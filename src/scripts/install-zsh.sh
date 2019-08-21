8dc1dd099396d422deee36cf470dced1ebc80247
elib0 <elijose.c@gmail.com>
2019-08-21 14:06:28 -0400

    - Se activan los plugins de oh-my-zsh por defecto

--- src/scripts/install-zsh.sh
+++ src/scripts/install-zsh.sh
@@ -19,7 +19,7 @@ git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-m
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
 # Activando plugins y configuraciones varias
-#plugins=(git zsh-autosuggestions zsh-syntax-highlighting)
+sudo sed -i 's/plugins=(git)/plugins=(git zsh-autosuggestions zsh-syntax-highlighting)/g' ~/.zshrc
 # Nombre de usuario del prompt
 echo "DEFAULT_USER=`whoami`" >> ~/.zshrc
 
